1b68008526f294a91a39dbca399e0784e8779435
Arin Ghazarian <aringhazarian@github.com>
2022-12-06 10:48:57 -0800

    Day 6

---
+++ day06/Program.cs
@@ -0,0 +1,33 @@
+﻿var data = File.ReadAllText("input.txt");
+
+long GetFirstMarkerIndex(int bufferSize)
+{
+    var buffer = new List<char>(bufferSize);
+    var number = 0L;
+    foreach (var c in data)
+    {
+        if (buffer.Count == bufferSize)
+        {
+            if (new HashSet<char>(buffer).Count == bufferSize)
+            {
+                return number;
+            }
+            
+            buffer.RemoveAt(0);
+        }
+
+        buffer.Add(c);
+        number++;
+    }
+
+    return data.LongCount();
+}
+
+long GetPart1() => GetFirstMarkerIndex(4);
+
+long GetPart2() => GetFirstMarkerIndex(14);
+
+Console.WriteLine($"Part 1: {GetPart1()}");
+Console.WriteLine($"Part 2: {GetPart2()}");
+
+Console.WriteLine("done");
